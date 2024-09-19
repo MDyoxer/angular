ac9c3237e45a2b210ac7aeab8e25f3a1221cf563
Giovani <gioelo@hotmail.com>
2024-09-18 18:16:34 -0700

    Otros cambios

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contador',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contador.component.html',
   styleUrl: './contador.component.css'
 })
